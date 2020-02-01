db69ea5cfeb460e426bd7df4baaff9b9e9aba6ef
StupidHobbit <3mir33@gmail.com>
2020-02-01 19:56:59 +0300

    CI hack

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from .actable import Actable
 from .action import ActionType, Action
 from .goal import GoalType, Goal
@@ -5,4 +7,8 @@ from .obstacle import Obstacle
 from .path import Path
 from .player import Player
 from .position import Position
-from .sprite import Sprite
+
+if os.environ.get('CI') is None:
+    from .sprite import Sprite
+else:
+    from .sprite_ci import Sprite
